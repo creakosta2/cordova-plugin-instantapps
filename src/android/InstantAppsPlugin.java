e70bd73ac48d3b9c930aa02bae8ee11454f27e92
creakosta <creakosta@mail.ru>
2020-04-16 22:34:21 +0300

    Add files via upload

--- src/android/InstantAppsPlugin.java
+++ src/android/InstantAppsPlugin.java
@@ -23,16 +23,34 @@ import com.google.android.gms.instantapps.InstantApps;
  */
 public class InstantAppsPlugin extends CordovaPlugin {
 	
+	/**
+	* Called when initializing the plugin
+	*
+	* @param cordova TODO
+	* @param webView TODO
+	*/
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
 	}
 	
+	/**
+	* Called when executing an action from the JS code
+	*
+	* @param action A string naming the action
+	* @param args JSON array containing all the arguments the action was called with
+	* @param callbackContext TODO
+	* @return true if success, false otherwise
+	*/
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         
 		if(action.equals("showInstallPrompt")) {
-			Intent postInstallIntent = null; //new Intent(cordova.getActivity().getApplicationContext(), cordova.getActivity()); //args.getString(0);
+			// TODO
+			//PackageManager packageManager = context.getPackageManager();
+			//Intent postInstallIntent = packageManager.getLaunchIntentForPackage(context.getPackageName());
+			//Intent postInstallIntent = new Intent(cordova.getActivity().getApplicationContext(), Intent.ACTION_MAIN); //args.getString(0);
+			Intent postInstallIntent = new Intent("android.intent.action.MAIN");
 			int requestCode = 7; //args.getInt(1);
 			String referrer = "InstantApps"; //args.getString(2);
 			return this.showInstallPrompt(postInstallIntent, requestCode, referrer, callbackContext);
@@ -41,6 +59,15 @@ public class InstantAppsPlugin extends CordovaPlugin {
         return false;
     }
 	
+	/**
+	* Opens a full app installation dialog
+	*
+	* @param postInstallIntent TODO
+	* @param requestCode An integer value containing the request code
+	* @param referrer A string containing the referrer
+	* @param callbackContext TODO
+	* @return true if success, false otherwise
+	*/
 	private boolean showInstallPrompt(Intent postInstallIntent, int requestCode, String referrer, CallbackContext callbackContext) {
 		if(InstantApps.showInstallPrompt(cordova.getActivity(), postInstallIntent, requestCode, referrer))
 		{
@@ -56,11 +83,8 @@ public class InstantAppsPlugin extends CordovaPlugin {
 
 	/*
     private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
+        if (message != null && message.length() > 0)
             callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
     }
 	*/
 }
